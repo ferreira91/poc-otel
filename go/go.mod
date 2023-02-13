002b5f4689097c6675a11d1a1ead5121ca98e17d
Caina Ferreira da Silva <caina.silva@zup.com.br>
2023-02-13 17:01:00 -0300

    get metrics from application

--- go/go.mod
+++ go/go.mod
@@ -4,6 +4,10 @@ go 1.19
 
 require (
 	github.com/go-resty/resty/v2 v2.7.0
+	github.com/labstack/echo-contrib v0.13.1
+	github.com/labstack/echo/v4 v4.10.0
+	github.com/prometheus/client_golang v1.14.0
+	github.com/spf13/viper v1.15.0
 	go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho v0.39.0
 	go.opentelemetry.io/otel v1.13.0
 	go.opentelemetry.io/otel/exporters/jaeger v1.13.0
@@ -12,13 +16,29 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/labstack/echo/v4 v4.10.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/spf13/afero v1.9.3 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.2.0 // indirect
@@ -26,4 +46,7 @@ require (
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	golang.org/x/time v0.2.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- go/main.go
+++ go/main.go
@@ -2,28 +2,100 @@ package main
 
 import (
 	"github.com/go-resty/resty/v2"
+	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
+	prom "github.com/prometheus/client_golang/prometheus"
+	"github.com/spf13/viper"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
 	"go.opentelemetry.io/otel/baggage"
 	"go.opentelemetry.io/otel/trace"
 	"log"
 	"main.go/infra"
+	"math/rand"
 	"net/http"
 	"time"
 )
 
 var tracer trace.Tracer
 
+const cKeyMetrics = "custom_metrics"
+
+type Metrics struct {
+	customCnt *prometheus.Metric
+	customDur *prometheus.Metric
+}
+
+// Needed by echo-contrib so echo can register and collect these metrics
+func (m *Metrics) MetricList() []*prometheus.Metric {
+	return []*prometheus.Metric{
+		// ADD EVERY METRIC HERE!
+		m.customCnt,
+		m.customDur,
+	}
+}
+
+// Creates and populates a new Metrics struct
+// This is where all the prometheus metrics, names and labels are specified
+func NewMetrics() *Metrics {
+	return &Metrics{
+		customCnt: &prometheus.Metric{
+			Name:        "custom_total",
+			Description: "Custom counter events.",
+			Type:        "counter_vec",
+			Args:        []string{"label_one", "label_two"},
+		},
+		customDur: &prometheus.Metric{
+			Name:        "custom_duration_seconds",
+			Description: "Custom duration observations.",
+			Type:        "histogram_vec",
+			Args:        []string{"label_one", "label_two"},
+		},
+	}
+}
+
+func (m *Metrics) AddCustomMetricsMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Set(cKeyMetrics, m)
+		return next(c)
+	}
+}
+
+func (m *Metrics) IncCustomCnt(labelOne, labelTwo string) {
+	labels := prom.Labels{"label_one": labelOne, "label_two": labelTwo}
+	m.customCnt.MetricCollector.(*prom.CounterVec).With(labels).Inc()
+}
+
+func (m *Metrics) ObserveCustomDur(labelOne, labelTwo string, d time.Duration) {
+	labels := prom.Labels{"label_one": labelOne, "label_two": labelTwo}
+	m.customCnt.MetricCollector.(*prom.HistogramVec).With(labels).Observe(d.Seconds())
+}
+
+
 func main() {
+	viper.SetDefault("JAEGER_ENDPOINT_EXPORTER", "http://localhost:14268/api/traces")
+	viper.SetDefault("CLIENT_ENDPOINT", "http://localhost:8080/otel")
+	viper.AutomaticEnv()
+
 	ot := infra.NewOtel()
 	ot.ServiceName = "goOtel"
-	ot.ExporterEndpoint = "http://localhost:14268/api/traces"
+	ot.ExporterEndpoint = viper.GetString("JAEGER_ENDPOINT_EXPORTER")
 	tracer = ot.GetTracer()
 
+	m := NewMetrics()
+
 	e := echo.New()
+	p := prometheus.NewPrometheus("echo", nil, m.MetricList())
+	p.Use(e)
+	e.Use(m.AddCustomMetricsMiddleware)
+
 	e.Use(otelecho.Middleware(ot.ServiceName))
 
 	e.GET("/test", func(c echo.Context) error {
+		options := []string{"metric", "trace", "log"}
+		rand.Seed(time.Now().UnixNano())
+		label := options[rand.Intn(len(options))]
+		metrics := c.Get(cKeyMetrics).(*Metrics)
+		metrics.IncCustomCnt(label, "value")
 		ctx := baggage.ContextWithoutBaggage(c.Request().Context())
 
 		// 1 - simulating the execution of something
@@ -48,7 +120,7 @@ func main() {
 
 func getTestOtel() string {
 	client := resty.New()
-	resp, err := client.R().EnableTrace().Get("http://localhost:8080/otel")
+	resp, err := client.R().EnableTrace().Get(viper.GetString("CLIENT_ENDPOINT"))
 	if err != nil {
 		log.Fatal(err)
 	}
